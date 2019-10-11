75c2bdf593d9e5d762b3ffb18997e957a6fc213b
Yasnia Deras Cruz <yasniad@hotmail.com>
2019-10-11 15:25:19 +0200

    C#

--- exercise2/Program.cs
+++ exercise2/Program.cs
@@ -1,1 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
 
+namespace Excersie_2 {
+    class Program {
+        static void Main(string[] args) {
+
+            var listInput = new List<string>();
+
+            var inpuut = true;
+            do {
+                Console.WriteLine("Lägg till en rad: ");
+                var input = Console.ReadLine();
+                if (input == "q" || input == "quit") {
+
+                    break;
+
+
+                } else {
+                    listInput.Add(input);
+                }
+
+            }
+            while (inpuut);
+
+            Console.WriteLine("Filter: ");
+            var filter = Console.ReadLine();
+            foreach (var listName in listInput) {
+                if(listName.StartsWith(filter)) {
+
+                    Console.WriteLine(listName);
+                }
+               
+
+            }
+
+            Console.ReadKey();
+            
+        }
+    }         
+}
+          
+
+        
+
+        
+   
