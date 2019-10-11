f1e900fd20f0c66049fbcd41034f3cb35e75e23c
Yasnia Deras Cruz <yasniad@hotmail.com>
2019-10-11 15:16:27 +0200

    C#

--- Program.cs
+++ Program.cs
@@ -1,59 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace ConsoleApp2 {
-    enum Mode {
-        Mulitply, Percentage
-    }
+namespace Excersie_2 {
     class Program {
         static void Main(string[] args) {
 
-            var mode = Mode.Mulitply; 
+            var listInput = new List<string>();
 
-            decimal X, Y;
+            var inpuut = true;
+            do {
+                Console.WriteLine("Lägg till en rad: ");
+                var input = Console.ReadLine();
+                if (input == "q" || input == "quit") {
 
-            Console.WriteLine("X:");
-            var inputX = Console.ReadLine();
-            Console.WriteLine("Y:");
-            var inputY = Console.ReadLine();
+                    break;
 
-
-
-            if (!decimal.TryParse(inputX, out X) || !decimal.TryParse(inputY, out Y)) {
-
-                Console.WriteLine("Error");
-            } else {
-
-                Console.WriteLine(@" Resultat " + X.ToString("N3") + "\n" + "\t" + Y.ToString("N3"));
-                decimal result;
-
-                Console.WriteLine("Mode: ");
-                var inputMode = Console.ReadLine();
-
-                if (inputMode == "percent") {
-                    mode = Mode.Percentage;
-                } else if (inputMode == "multiply") {
-                    mode = Mode.Mulitply;
-                }
-
-                
-                if (mode == Mode.Mulitply) {
-                    result = X * Y;
 
                 } else {
-                    result = X / Y; 
+                    listInput.Add(input);
                 }
 
-                Console.WriteLine("Resultat: " + result.ToString(mode == Mode.Percentage ? "P2" : "N3")); 
+            }
+            while (inpuut);
+
+            Console.WriteLine("Filter: ");
+            var filter = Console.ReadLine();
+            foreach (var listName in listInput) {
+                if(listName.StartsWith(filter)) {
+
+                    Console.WriteLine(listName);
+                }
+               
 
             }
+
             Console.ReadKey();
-
-
-
-
+            
+        }
+    }         
+}
+          
 
         
 
-        }
-    }
-}
+        
+   
